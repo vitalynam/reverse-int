27ded42f7e4071778d7748b59c4bbb87a5e824d9
Namiko01 <vitaliknam01@gmail.com>
2023-09-25 22:49:21 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    
+    let str = n.toString();
+    let arr = [];
+    let result = '';
+    
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === '-'){
+            continue;
+        }else{
+            arr.push(str[i]);
+        }
+    }
+
+    result = arr.reverse().join('');
+    return +result;
 }
